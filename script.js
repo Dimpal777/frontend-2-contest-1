c5e389dd9871264afedd1e355e79b08d78c2c8fe
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2023-01-07 19:07:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,63 @@
 /** @format */
 
 let arr = [
-  { id: 1, name: "john", age: "18", profession: "developer" },
-  { id: 2, name: "jack", age: "20", profession: "developer" },
-  { id: 3, name: "karen", age: "19", profession: "admin" },
+  { id: 10, name: "Dhiraj", age: "30", profession: "support developer" },
+  { id: 9, name: "Niraj", age: "22", profession: "developer" },
+  { id: 8, name: "Suraj", age: "40", profession: "ceo" },
 ];
 
 function PrintDeveloperswithMap() {
   //Write your code here
+   function printingThroghMap(arrayItem) {
+    if (arrayItem.profession === "support developer") {
+      console.log(arrayItem);
+    }
+  }
 }
+  
+
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+function printingThroghForEach(arrayItem) {
+    if (arrayItem.profession === "developer ") {
+      console.log(arrayItem);
+    }
+  }
 }
+
+
 
 function addData() {
   //Write your code here
+  let newObj = { id: 7, name: "susant", age: "25", profession: "internship" };
+  arr.push(newObj);
+  console.log(arr);
 }
+
+
 
 function removeAdmin() {
   //Write your code here
+   let filtered_arr = arr.filter(function (val) {
+    if (val.profession !== "admin") {
+      return val;
+    }
+  });
+  console.log(filtered_arr);
 }
+
+
 
 function concatenateArray() {
   //Write your code here
+   let arr2 = [
+    { id: 6, name: "Dimpal", age: "21", profession: "support enginer" },
+    { id: 5, name: "Aaru", age: "26", profession: "worker" },
+    { id: 4, name: "Ruhi", age: "31", profession: "manager " },
+  ];
+ 
+  
+  let concatArray = arr.concat(arr2);
+  console.log(concatArray);
 }
